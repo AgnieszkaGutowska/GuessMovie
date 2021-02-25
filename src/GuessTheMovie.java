0db9a7c7a38e8b30e6aacfa17d76d6d59962bccc
Agnieszka Gutowska <a.gutowska1@gmail.com>
2021-02-25 17:42:37 +0100

    test

---
+++ src/GuessTheMovie.java
@@ -0,0 +1,38 @@
+
+import java.io.File;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class GuessTheMovie {
+
+    public static void main(String [] args ) throws Exception{
+        File file=new File ("movies.txt");
+        Scanner scanner = new Scanner(file);
+        Scanner scanner2 = new Scanner(file);
+
+        int count=0;
+        while(scanner.hasNextLine()){
+            scanner.nextLine();
+            count++;
+        }
+        System.out.println(count);// wyświetli listę
+
+        String films[] = new String[count];
+
+        count = 0;
+        while(scanner2.hasNextLine()){
+            String line = scanner2.nextLine();
+            films[count] = line;
+            count++;
+           // System.out.println(line.split("").length); //długość nazw
+        }
+
+        System.out.println(films[8]);
+
+
+    }
+
+
+}
+
