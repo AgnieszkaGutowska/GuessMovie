bb0fedfbc61af301095c282f059d552980cb6dfc
Agnieszka Gutowska <a.gutowska1@gmail.com>
2021-02-25 21:55:31 +0100

    test

--- src/GuessTheMovie.java
+++ src/GuessTheMovie.java
@@ -29,8 +29,41 @@ public class GuessTheMovie {
         }
 
         Random random= new Random();
+        String cinema=films[random.nextInt(count)];
+        //System.out.println(films[random.nextInt(count)]); //długość nazw
+        System.out.println(cinema);
+        char[] cinemaArray = cinema.toCharArray();
+        System.out.println(cinemaArray);
 
-        System.out.println(films[random.nextInt(count)]); //długość nazw
+        System.out.println("System wybrał tytuł filmu: ");
+        String zmiana = cinema.replaceAll("[a-z]","_");
+        char[] zmianaArray = zmiana.toCharArray();
+        System.out.println( "Spróbuj zgadnąć:  ");
+
+        Scanner scanner1 = new Scanner(System.in);
+        for (int i=10; i>0; i--){
+            System.out.println("Masz " + i + " prób." );
+          String guess = scanner1.nextLine();
+          int a = cinema.indexOf(guess);
+               // System.out.println(a);
+
+                    if(a==-1){
+                        System.out.println("brak");
+                    }
+                    else {
+                       System.out.println(a);
+                       zmianaArray[a]=guess.charAt(0);
+                       System.out.println(zmianaArray);
+                    }
+
+        }
+
+
+
+
+
+
+
 
 
     }
